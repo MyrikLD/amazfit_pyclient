a18dcf6255e1c3730a463f47ba4e8c1aaf48babc
Myrik <myrik260138@gmail.com>
2024-03-31 19:07:11 +0200

    fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="amazfit_pyclient",
-    version="0.1",  # Update with your project version
+    version="0.2",  # Update with your project version
     packages=find_packages(
         exclude=["tests"],
     ),
